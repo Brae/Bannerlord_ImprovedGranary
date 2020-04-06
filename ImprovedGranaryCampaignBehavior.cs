52efe748b788a06075885b18d81c2bd2721bd5d1
Brae <brae@hatching.io>
2020-04-06 22:31:55 +0200

    Add project files.

---
+++ ImprovedGranaryCampaignBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace ImprovedGranary.CampaignBehavior
+{
+    internal class ImprovedGranaryCampaignBehavior : CampaignBehaviorBase
+    {
+        public ItemRoster granaryRoster = new ItemRoster();
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnNewGameCreatedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(this.AddGameMenus));
+            CampaignEvents.OnGameLoadedEvent.AddNonSerializedListener(this, new Action<CampaignGameStarter>(this.AddGameMenus));            
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+            
+        }
+
+        protected void AddGameMenus(CampaignGameStarter campaignGameSystemStarter)
+        {
+            InformationManager.DisplayMessage(new InformationMessage("Loaded Improved Granary mod"));
+            campaignGameSystemStarter.AddGameMenuOption("town", "town_granary", "Open granary", new GameMenuOption.OnConditionDelegate(ImprovedGranaryCampaignBehavior.town_menu_granary_on_condition), new GameMenuOption.OnConsequenceDelegate(this.town_menu_granary_on_consequence), false, -1, false);
+        }
+        
+        private static bool town_menu_granary_on_condition(MenuCallbackArgs args)
+        {
+            args.optionLeaveType = GameMenuOption.LeaveType.Trade;
+            return Settlement.CurrentSettlement.OwnerClan == Clan.PlayerClan;
+        }
+
+        private void town_menu_granary_on_consequence(MenuCallbackArgs args)
+        {
+            LocationEncounter locationEncounter = PlayerEncounter.LocationEncounter;
+            InventoryManager.OpenScreenAsTrade(this.granaryRoster, Settlement.CurrentSettlement.GetComponent<Town>(), InventoryManager.InventoryCategoryType.Goods, new InventoryManager.DoneLogicExtrasDelegate(this.doneStashDelegate));
+            //InventoryManager.OpenScreenAsStash(new ItemRoster());            
+        }
+
+        private void doneStashDelegate()
+        {
+            Settlement.CurrentSettlement.Town.FoodStocks = Settlement.CurrentSettlement.Town.FoodStocks + this.granaryRoster.TotalFood;
+        }
+    }
+ }
---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using ImprovedGranary.CampaignBehavior;
+
+namespace ImprovedGranary
+{
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            Campaign campaign = game.GameType as Campaign;
+            bool flag = campaign == null;
+            if (!flag)
+            {
+                CampaignGameStarter gameInitializer = (CampaignGameStarter)gameStarterObject;
+                gameInitializer.AddBehavior(new ImprovedGranaryCampaignBehavior());
+            }
+        }
+    }
+}
